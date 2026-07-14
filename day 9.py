bd90f7b4e1affc135faa164c6113fd17900e7e9f
Asmita Jamge <asmitajamge@gmail.com>
2026-07-14 15:53:16 +0530

    day 9: Added loop programs

---
+++ day 9.py
@@ -0,0 +1,34 @@
+for i in range(6):
+    for j in range(1, 6):
+        print("*", end=" ")
+    print()
+
+
+print("=" * 30)
+
+for i in range(1, 3):
+    for j in range(1, 3):
+        print(i, j)
+
+print("=" * 30)
+
+for i in range(1, 5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+
+print("=" *30)
+
+
+for i in range(5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+print("="*30)
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print("*", end=" ")
+    print()
