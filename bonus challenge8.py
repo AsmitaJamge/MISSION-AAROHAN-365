f747ea8b3823c17586d2d74baed66bd54916425a
Asmita Jamge <asmitajamge@gmail.com>
2026-07-15 18:16:47 +0530

    Python-Sets-All-Concepts

---
+++ bonus challenge8.py
@@ -0,0 +1,18 @@
+# Create Sets
+A = {10, 20, 30, 40, 50}
+B = {30, 40, 50, 60, 70}
+
+# Union
+print("Union:", A | B)
+
+# Intersection
+print("Intersection:", A & B)
+
+# Difference (A - B)
+print("Difference (A - B):", A - B)
+
+# Difference (B - A)
+print("Difference (B - A):", B - A)
+
+# Symmetric Difference
+print("Symmetric Difference:", A ^ B)
